e99ae1580cf836e943d536a95517e631ec1126fe
titibs29 <thibaut.rizzoli@hotmail.be>
2022-05-24 16:27:06 +0200

    ajout des librairies

---
+++ main.py
@@ -0,0 +1,19 @@
+# imports et pré exec
+import matplotlib as plt
+import csv
+
+
+def decompose(input_file,output_file):
+    # lecture des entrees
+    print("hii")
+
+    # algo
+
+    # sortie
+
+
+
+if __name__ == "__main__":
+    # boucle si lecture seule
+    print("lecture du fichier input.txt")
+    decompose("input.txt","output.txt")
