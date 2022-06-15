23b35f6a59e7c425156b4eb948303d7bcd4e640f
titibs29 <thibaut.rizzoli@hotmail.be>
2022-06-15 21:31:58 +0200

    begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from signals import convToFourier
 
-#signal à convertir
+#signal à convertir (écart de temps: 1)
 signal =[5,5,5,5,5,-5,-5,-5,-5,-5,5,5,5,5,5,-5,-5,-5,-5,-5,5,5,5,5,5,-5,-5,-5,-5,-5,5,5,5,5,5,-5,-5,-5,-5,-5,5,5,5,5,5,-5,-5,-5,-5,-5,5,5,5,5,5,-5,-5,-5,-5,-5,5,5,5,5,5,-5,-5,-5,-5,-5,5,5,5,5,5,-5,-5,-5,-5,-5,]
 
 
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     # boucle principale
     fourier = []
     fourier = convToFourier(signal)
+    print(fourier)
--- signals.py
+++ signals.py
@@ -1,3 +1,3 @@
 # fonction qui récupère un signal et le converti en série de fourier
 def convToFourier(input):
-    
+    #conditions: non sinusoidale, continue, périodique entre -PI et PI
